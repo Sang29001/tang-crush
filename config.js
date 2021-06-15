a45fa99759b7416a6fa4f92286e42aa2fdaf778a
Sang29001 <85912403+Sang29001@users.noreply.github.com>
2021-06-15 12:28:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,6 @@
 const CONFIG = {
-    introTitle: 'Chị à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Cục cưng à',
+    introDesc: `Trái đất này vốn nh,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
